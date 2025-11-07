9b2e0a09dd8d7bd92aa8a6543428061a474b541e
rjlmax <158027969@qq.com>
2025-11-07 23:53:57 +0800

    week02

--- 饶建立/week02/作业1.py
+++ 饶建立/week02/作业1.py
@@ -17,7 +17,7 @@ class Torchmodel(nn.Module):
 
     def forward(self,x,y=None):
         y_pre=self.linear(x)
-        if y:
+        if y is not None:
             loss=self.loss(y_pre,y)
             return y_pre,loss
         else:
@@ -25,55 +25,29 @@ class Torchmodel(nn.Module):
 
 
 # 随机生成训练样本，构造数据
-class TrainDataset(Dataset):
+class SampleDataset(Dataset):
     def __init__(self,train_num_samples,nun_vector):
         self.nun_vector = nun_vector
         self.num_samples=train_num_samples
-        self.X,self.Y=self._generate_sample()
 
-    def _generate_sample(self):
-        X=[]
-        Y=[]
-        for _ in range(self.num_samples):
-            x=np.random.randn(self.nun_vector)
-            y=np.argmax(x)
-            X.append(x)
-            Y.append(y)
-        return torch.FloatTensor(X),torch.LongTensor(Y)
+
     def __len__(self):
         return self.num_samples
 
     def __getitem__(self, idx):
-        return self.X[idx],self.Y[idx]
-
-
-# 随机生成测试样本，构造数据
-class TsetDataset(Dataset):
-    def __init__(self, test_num_samples,nun_vector):
-        self.nun_vector=nun_vector
-        self.num_samples = test_num_samples
-        self.X, self.Y = self._generate_sample()
-
-    def _generate_sample(self):
-        X = []
-        for _ in range(self.num_samples):
-            x = np.random.randn(self.nun_vector)
-            X.append(x)
-        return torch.FloatTensor(X)
-    def __len__(self):
-        return self.num_samples
-
-    def __getitem__(self, idx):
-        return self.X[idx]
+        x=np.random.randn(self.nun_vector).astype('float32')
+        y=np.argmax(x)
+        return torch.from_numpy(x),y
 
 
 
-def train_model_process(model,train_dataloader,num_epochs):
+def train_model_process(model,train_dataloader,num_epochs,model_path):
     # 设定训练设备
     device=torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print(device)
     # 指定优化器,损失函数
     optimizer=torch.optim.Adam(model.parameters(),lr=0.001)
-    criterion=nn.CrossEntropyLoss()
+
     # 将模型放入设备中
     model=model.to(device)
 
@@ -89,7 +63,7 @@ def train_model_process(model,train_dataloader,num_epochs):
             model.train()
             # 前向传播,计算损失
             y_pre,loss=model(b_x,b_y)
-            pre_label=torch.argmax(torch.softmax(y_pre,dim=1),dim=1)
+            pre_label=torch.argmax(y_pre,dim=1)
 
             # 反向传播，梯度更新，清空梯度
             loss.backward()
@@ -97,26 +71,76 @@ def train_model_process(model,train_dataloader,num_epochs):
             optimizer.zero_grad()
             # 记录损失
             watch_loss.append(loss.item())
-            train_corrects+=torch.sum(pre_label==b_y)
+            train_corrects+=torch.sum(pre_label==b_y).item()
             train_num += b_x.size(0)
-        print(f"epoch:{epoch},loss:{np.mean(watch_loss)}")
-        print(f"train_num:{train_num},train_corrects:{train_corrects}")
-
-
-model=Torchmodel(5,5)
-
-train_data=TrainDataset(train_num_samples=1000,nun_vector=5)
-
-dataloader = DataLoader(
-    train_data,
-    batch_size=32,
-    shuffle=True,
-    num_workers=2  # 多进程加载数据
-)
+        print(f"epoch:{epoch},loss:{np.mean(watch_loss)},acc:{train_corrects/train_num}")
+        torch.save(model.state_dict(),model_path)
 
 
 
-train_model_process(model,dataloader,20)
+
+def predict(model_path,test_dataloader):
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print(device)
+    model.load_state_dict(torch.load(model_path))
+    criterion = nn.CrossEntropyLoss()
+    model.eval()
+    test_loss=[]
+    test_corrects=0
+    test_num=0
+    with torch.no_grad():
+        for batch_idx, (batch_x, batch_y) in enumerate(test_dataloader):
+            b_x=batch_x.to(device)
+            b_y=batch_y.to(device)
+            y_pre=model(b_x)
+            pre_label=torch.argmax(y_pre,dim=1)
+            # 记录损失
+            loss=criterion(y_pre,b_y)
+            test_loss.append(loss.item())
+            test_corrects+=torch.sum(pre_label==b_y).item()
+            test_num+=b_x.size(0)
+        print(f"loss:{np.mean(test_loss)},acc:{test_corrects/test_num}")
+
+
+
+
+
+
+
+
+if __name__=="__main__":
+    # windows加载多进程必须的
+    torch.multiprocessing.freeze_support()
+
+    model=Torchmodel(5,5)
+
+    train_data=SampleDataset(train_num_samples=10000,nun_vector=5)
+
+    dataloader = DataLoader(
+        train_data,
+        batch_size=100,
+        shuffle=True,
+        num_workers=2,  # 多进程加载数据
+        pin_memory=True  # 加速GPU传输数据
+    )
+    model_path="./model.pth"
+    train_model_process(model,dataloader,20,model_path)
+
+    print("="*10+"predict"+"="*10)
+
+    test_data = SampleDataset(train_num_samples=10000, nun_vector=5)
+    test_dataloader = DataLoader(
+        test_data,
+        batch_size=100,
+        shuffle=True,
+        num_workers=2,  # 多进程加载数据
+        pin_memory=True  # 加速GPU传输数据
+    )
+
+    predict(model_path, test_dataloader)
+
+
+
 
 
 
